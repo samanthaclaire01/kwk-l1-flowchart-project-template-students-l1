750ed166c1c8b470d7ea77a121d0b0952418f2ce
Samantha Cassity <cassityclaire@gmail.com>
2018-06-15 15:32:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
+
 class ApplicationController < Sinatra::Base
+
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/' do
+    the_occasion = params[:occasion]
+    the_gender = params[:gender]
+    the_weather = params[:weather]
+    @result_stuff = check(the_occasion, the_gender, the_weather)
+    erb :results
+  end
+  
+  # OCCASION GENDER WEATHER
+
 
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,45 @@
-class Quiz
 
-end
+def check(occasionn, gender, weather)
+occasion={
+  "formal"=>{
+    "feminine"=>{
+      "hot"=> ["Formal shirt", 
+      "https://www.jcpenney.com/p/levis-liza-tie-shirt/ppr5007667215?pTmplType=regular&rrplacementtype=cretio_zone&cm_re=Hooklogic-_-Hooklogic-_-Hooklogic",
+      "Formal shorts",
+      "https://www.jcpenney.com/p/liz-claiborne-classic-fit-secretly-slender-pull-on-pants/ppr5007237191?pTmplType=regular&catId=cat100250095&deptId=dept20000013&urlState=/g/womens-pants/N-bwo3xD1nopgv&productGridView=medium&badge=onlyatjcp",
+      "Formal dress",
+      "https://www.lulus.com/products/lulus-exclusive-nice-touch-black-dress/173354.html"],
+      "cold"=> "This is what we have found for you -cold",
+      "warm"=> "This is what we have found for you -warm",
+    },
+    "masculine"=>{
+      "hot"=> "This is what we have found for you-hot - hot",
+      "cold"=> "This is what we have found for you -cold",
+      "warm"=> "This is what we have found for you -warm",
+    },
+    },
+  "casual"=>{
+  "feminine"=>{
+    "hot"=> "This is what we have foudn for you ",
+    "cold"=> "This is what we have found for you",
+    "warm"=> "This is what we have found for you",
+    },
+  "masculine"=>{
+    "hot"=> "casual -hot",
+    "cold"=>"casual -cold",
+    "warm"=>"casual -warm",
+  },
+  }, 
+}
+
+
+result = occasion[occasionn][gender][weather]
+
+ 
+end 
+
+ 
+# puts check("formal","feminine","hot")
+
+
+
