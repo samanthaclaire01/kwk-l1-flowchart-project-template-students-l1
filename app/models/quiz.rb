4386b593d4769a444b893689b053dc61ec771515
Samantha Cassity <cassityclaire@gmail.com>
2018-06-15 16:45:43 +0000

    automatically backed up by learn

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,45 +1,40 @@
 
-def check(occasionn, gender, weather)
-occasion={
-  "formal"=>{
-    "feminine"=>{
-      "hot"=> ["Formal shirt", 
-      "https://www.jcpenney.com/p/levis-liza-tie-shirt/ppr5007667215?pTmplType=regular&rrplacementtype=cretio_zone&cm_re=Hooklogic-_-Hooklogic-_-Hooklogic",
-      "Formal shorts",
-      "https://www.jcpenney.com/p/liz-claiborne-classic-fit-secretly-slender-pull-on-pants/ppr5007237191?pTmplType=regular&catId=cat100250095&deptId=dept20000013&urlState=/g/womens-pants/N-bwo3xD1nopgv&productGridView=medium&badge=onlyatjcp",
-      "Formal dress",
-      "https://www.lulus.com/products/lulus-exclusive-nice-touch-black-dress/173354.html"],
-      "cold"=> "This is what we have found for you -cold",
-      "warm"=> "This is what we have found for you -warm",
+# def check(occasionn, gender, weather)
+$that_hash = {
+  "formal" => {
+    "feminine" => {
+        # for each result you should format the items in the array like item, link, photo, item, link, photo, etc
+      "hot" => ["Formal dress",
+      "https://www.lulus.com/products/lulus-exclusive-nice-touch-black-dress/173354.html",
+      "https://www.lulus.com/images/product/xlarge/1108186_173354.jpg"],
+      "cold" => "This is what we have found for you -cold",
+      "warm" => "This is what we have found for you -warm",
     },
-    "masculine"=>{
-      "hot"=> "This is what we have found for you-hot - hot",
-      "cold"=> "This is what we have found for you -cold",
-      "warm"=> "This is what we have found for you -warm",
+    "masculine" => {
+      "hot" => "This is what we have found for you-hot - hot",
+      "cold" => "This is what we have found for you -cold",
+      "warm" => "This is what we have found for you -warm",
     },
     },
-  "casual"=>{
-  "feminine"=>{
-    "hot"=> "This is what we have foudn for you ",
-    "cold"=> "This is what we have found for you",
-    "warm"=> "This is what we have found for you",
+  "casual" => {
+  "feminine" => {
+    "hot" => "This is what we have foudn for you ",
+    "cold" => "This is what we have found for you",
+    "warm" => "This is what we have found for you",
     },
-  "masculine"=>{
-    "hot"=> "casual -hot",
-    "cold"=>"casual -cold",
-    "warm"=>"casual -warm",
+  "masculine" => {
+    "hot" => "casual -hot",
+    "cold" => "casual -cold",
+    "warm" => "casual -warm",
   },
   }, 
 }
 
 
-result = occasion[occasionn][gender][weather]
-
+# occasion[occasionn][gender][weather]
  
-end 
+# end 
 
- 
-# puts check("formal","feminine","hot")
-
-
-
+def get_outfit(occasion, gender, weather)
+    $that_hash[occasion][gender][weather]
+end
